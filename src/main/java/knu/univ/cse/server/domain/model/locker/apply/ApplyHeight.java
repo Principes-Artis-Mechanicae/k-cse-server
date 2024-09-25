229a727c87f2c8c4a613d66ef11192e71ffe0696
lsmin3388 <lsmin3388_@naver.com>
2024-09-25 15:24:29 +0900

    fix: 사물함 TOP, BOTTOM 변경

--- src/main/java/knu/univ/cse/server/domain/model/locker/apply/ApplyHeight.java
+++ src/main/java/knu/univ/cse/server/domain/model/locker/apply/ApplyHeight.java
@@ -10,14 +10,14 @@ public enum ApplyHeight {
 	public List<Integer> getLockerHeight(LockerFloor floor) {
 		return switch (floor) {
 			case FLOOR_L, FLOOR_B1 -> switch (this) {
-				case TOP -> List.of(3, 4);
+				case TOP -> List.of(2, 1);
 				case MIDDLE -> List.of(2, 3);
-				case BOTTOM -> List.of(1, 2);
+				case BOTTOM -> List.of(3, 4);
 			};
 			case FLOOR_3F -> switch (this) {
-				case TOP -> List.of(4, 5);
+				case TOP -> List.of(1, 2);
 				case MIDDLE -> List.of(3, 4);
-				case BOTTOM -> List.of(1, 2);
+				case BOTTOM -> List.of(4, 5);
 			};
 		};
 	}
